5d4d1882457c612faf2a099c9726066764c72716
RicardoJun10r <ricardojunior0110@gmail.com>
2022-08-12 18:45:13 -0300

    implementando a tela login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,12 +1,67 @@
+import { useState } from 'react';
 import './style.css';
 
+const initialValues = {
+    email: '',
+    senha: '',
+}
+
 function Login() {
+
+    const[values, setValues] = useState(initialValues);
+    const[cont, setCont] = useState(0);
+
+    function handleChange(e){
+        const { name, value } = e.target;
+        setValues({
+            ...values, [name]: value
+        });
+    }
+    function onSubmit(e)
+    {
+        e.preventDefault();
+
+        setCont(cont+1);
+        console.log(cont);
+    }
+
     return(
         <div>
            <div className='container'>
                 <div className='container_inner'>
                     <div className='sloga'>
+                        {/*Lado esquerdo*/}
                         <h1>Armazenar é bom, organizar é genial</h1>
+                    </div>
+                    <div className='container-form'>
+                        {/*Lado direito*/}
+                        <h1>Log in</h1>
+                        <form>
+                            <label className='email-label'>USUÁRIO</label>
+                            <br />
+                            <input 
+                            className='email-input' 
+                            type="email"
+                            id='email'
+                            value={values.email}
+                            onChange={handleChange}
+                            />
+                            <br />
+                            <br />
+                            <label className='senha-label'>SENHA</label>
+                            <br />
+                            <input 
+                            className='senha-input' 
+                            type="password" 
+                            id='senha'
+                            autoComplete='off'
+                            value={values.senha}
+                            onChange={handleChange}
+                            />
+                            <br />
+                            <br />
+                            <button className='entrar-button' type='submit' onClick={onSubmit}>ENTRAR</button>
+                        </form>
                     </div>
                 </div>
            </div>
