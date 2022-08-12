e3f2cb38b75b9e1aa9896b46d3e1f8e1655f4b9e
RicardoJun10r <ricardojunior0110@gmail.com>
2022-08-12 12:28:05 -0300

    add pagina de cadastro e removendo arquivo css global

---
+++ src/pages/Cadastrar/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Cadastrar = () => {
+
+    return(
+        <div>
+            <h1>
+                Cadastro
+            </h1>
+        </div>
+    )
+}
+
+export default Cadastrar;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 import Login from "../pages/Login";
+import Cadastrar from "../pages/Cadastrar";
 
 function Rotas() {
     return(
         <BrowserRouter>
         <Routes>
             <Route element={<Login />} path='/' exact />
+            <Route element={<Cadastrar />} path='/Cadastrar'/>
         </Routes>
         </BrowserRouter>
     )
