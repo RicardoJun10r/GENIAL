d3aeb73b185e08a421f7b92c824f24fe45e45bad
vitordbo <vitorduarte177@gmail.com>
2022-08-22 18:53:16 -0300

    começo modal

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,6 +1,8 @@
 import './home.css';
 import armazenLogo from "../../assets/warehouse.png";
 import Container from 'react-bootstrap/esm/Container';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 function Home() {
 
@@ -10,10 +12,23 @@ function Home() {
                 <div>
                     <h2><img src={armazenLogo} alt='ArmazenLogo' className='ArmazenLogo' /> Genial</h2>
                 </div>
-                <div className='container'>
-                    <h3>Main</h3>  
+                <div className='modal'>
+                    <Modal.Dialog>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Modal title</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <p>Modal body text goes here.</p>
+                        </Modal.Body>
+
+                        <Modal.Footer>
+                            <Button variant="secondary">Close</Button>
+                            <Button variant="primary">Save changes</Button>
+                        </Modal.Footer>
+                    </Modal.Dialog>
                 </div>
             </div>
+    
         </Container>
     )
 }
