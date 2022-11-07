0995ea97a236b74ab750cf7cfa5dd1ecc3a51642
RicardoJun10r <ricardojunior0110@gmail.com>
2022-11-07 09:45:16 -0300

    att

--- src/pages/Produto/produtos.js
+++ src/pages/Produto/produtos.js
@@ -1,10 +1,34 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import database from '../../services/api';
 
-const Produtos = ({ produtos }) => {
+const Produtos = () => {
+
+    const [storageIndex, setStorageIndex] = useState(null)
+    const [products, setProducts] = useState([]);
+
+    const getProducts = () => {
+        let index = localStorage.getItem('storage')
+        return JSON.parse(index)
+    }
+
+    const storage = database.filter(storage => storage.id == storageIndex);
+
+    useEffect(()=>{
+        setStorageIndex(getProducts())
+        setProducts(storage.produtos)
+    },[storageIndex, products])
 
     return(
         <div>
             <h1>Ola</h1>
+            <div>
+                {products.map((produto) => {
+                    return(
+                        <p key={produto.id}>{produto.nome} + {}</p>
+                    )
+                })}
+            </div>
         </div>
     )
 }
